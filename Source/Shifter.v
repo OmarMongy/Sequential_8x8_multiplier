c504ddb1479f4101cb48878414417762ee0ab81b
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-07-13 20:00:24 +0300

    Create Shifter.v

---
+++ Source/Shifter.v
@@ -0,0 +1,17 @@
+module Shifter (
+  input [7:0] inp,
+  input [1:0] shift_cntrl,
+  output reg [15:0] shift_out
+);
+
+
+  always @(*) begin
+    case(shift_cntrl)
+      2'b00: shift_out = {8'b0000_0000, inp};
+      2'b01: shift_out = {4'b0000, inp, 4'b0000};
+      2'b10: shift_out = {inp, 8'b0000_0000};
+      2'b11: shift_out = {8'b0000_0000, inp};
+    endcase
+  end
+
+endmodule
