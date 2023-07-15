0cb4de19c1c1a575fd5c3609f268c1dad703e7d5
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-07-15 14:15:12 +0300

    Update Multiplier_controller._tb.v

--- Sim/Multiplier_controller._tb.v
+++ Sim/Multiplier_controller._tb.v
@@ -1,87 +1,61 @@
 module mult_control_TestBench;
 
-    // Inputs
-    reg clk;
-    reg reset_a;
-    reg start;
-    reg [1:0] count;
+reg clk, reset_a, start;
+   reg [1:0] count;        
+   wire done,clk_ena,sclr_n;
+   wire [2:0] state_out;
+   wire [1:0] input_sel,shift_sel;    
 
-    // Outputs
-    wire done;
-    wire clk_ena;
-    wire sclr_n;
-    wire [2:0] state_out;
-    wire [1:0] input_sel;
-    wire [1:0] shift_sel;
+   mult_control DUT (
+   .clk(clk),
+   .reset_a(reset_a),
+   .start(start),
+   .count(count),
+   .done(done),
+   .clk_ena(clk_ena),
+   .sclr_n(sclr_n),
+   .state_out(state_out),
+   .input_sel(input_sel),
+   .shift_sel(shift_sel)  
+);
 
-    // Instantiate the Unit Under Test (UUT)
-    mult_control uut (
-        .clk(clk), 
-        .reset_a(reset_a), 
-        .start(start), 
-        .count(count), 
-        .done(done), 
-        .clk_ena(clk_ena), 
-        .sclr_n(sclr_n), 
-        .state_out(state_out), 
-        .input_sel(input_sel), 
-        .shift_sel(shift_sel)
-    );
-
-    initial begin
-        // Initialize Inputs
-        clk = 0;
-        reset_a = 0;
-        start = 0;
-        count = 0;
-
-        // Wait for 100 ns for global reset to finish
-        #100;
-        
-        // De-assert reset
-        reset_a = 1;
-        #100;
-
-        // Assert start and provide count
-        start = 1;
-        count = 2'b10;
-        #100;
-
-        // De-assert start
-        start = 0;
-        #100;
-
-        // Assert start and provide count
-        start = 1;
-        count = 2'b11;
-        #100;
-
-        // De-assert start
-        start = 0;
-        #100;
-
-        // Assert start and provide count
-        start = 1;
-        count = 2'b01;
-        #100;
-
-        // De-assert start
-        start = 0;
-        #100;
-
-        // Assert start and provide count
-        start = 1;
-        count = 2'b00;
-        #100;
-
-        // De-assert start
-        start = 0;
-        #100;
-
-        // Finish the simulation
-        $finish;
+    always begin
+        clk = 1'b1;
+        #25;
+        clk = 1'b0;
+        #25;
     end
 
-    always #10 clk = ~clk;
+    initial begin
+        reset_a = 1'b0;
+        #75;
+        reset_a = 1'b1;
+    end
 
+   initial begin
+
+       start = 0;
+       #50;
+       @(negedge clk);
+       start = 1'b1;
+       #50;
+       @(negedge clk);
+       start = 1'b0;
+       count= 2'b00;
+       #50;
+       @(negedge clk);
+       count= 2'b01;
+       #50; 
+      @(negedge clk);
+       count= 2'b10;
+       #50;
+       @(negedge clk);
+       count=2'b11;
+       #50;
+       start = 1'b1;
+       #50;
+      
+   end
 endmodule
+
+
